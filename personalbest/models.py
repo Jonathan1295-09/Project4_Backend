1d3fc093ae3cb2dd408245d5844b0d4e89640170
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-25 21:34:03 -0500

    models.py made

--- personalbest/models.py
+++ personalbest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class personalBest(models.Model):
+    Cardio = models.CharField(max_length=200)
+    Benchpress = models.CharField(max_length=200)
+    Deadlift = models.CharField(max_length=200)
+    Squat = models.CharField(max_length=200)
+    Shoulderpress = models.CharField(max_length=200)
